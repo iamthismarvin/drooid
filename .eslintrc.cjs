b48364a208fb21c2b82e2ebb9b58cdfa216ce0e3
Marvin Parada <hello@marvin.dev>
2023-05-07 17:49:34 -0700

    Install ESLint.

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'plugin:react/jsx-runtime'
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: 'tsconfig.json',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  ignorePatterns: ['vite.config.ts', 'src/vite-env.d.ts'],
+  plugins: ['react'],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  rules: {
+    '@typescript-eslint/no-floating-promises': ['error']
+  }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import { useEffect, useState } from "react";
-import { invoke } from "@tauri-apps/api/tauri";
+import { type FC, useEffect, useState } from 'react'
+import { invoke } from '@tauri-apps/api/tauri'
 
-function App() {
-  const [greetMsg, setGreetMsg] = useState("");
+const App: FC = () => {
+  const [greetMsg, setGreetMsg] = useState('')
 
   useEffect(() => {
-    greet("Drooid");
-  }, []);
+    void greet('Drooid')
+  }, [])
 
-  async function greet(name: String) {
+  async function greet (name: string): Promise<void> {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
+    setGreetMsg(await invoke('greet', { name }))
   }
 
   return (
     <div>
       <h1 className="font-bold">{greetMsg}</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

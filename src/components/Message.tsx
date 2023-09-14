1b919e5565e8d7edebb5409891a33a43c469647f
Marvin Parada <hello@marvin.dev>
2023-09-14 12:54:55 -0700

    Add HuggingFace Inference package.

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,4 +1,4 @@
-import React, { type FC } from 'react'
+import { type FC } from 'react'
 
 export interface MessageProps {
   createdAt: string
@@ -7,16 +7,46 @@ export interface MessageProps {
 }
 
 const Message: FC<MessageProps> = ({ createdBy, message }) => {
-  const parsedText = message.split('\n')
+  const formatResponse: (response: string) => JSX.Element[] = (
+    response: string
+  ) => {
+    const lines = response.split('\n')
+    const formattedLines = []
+    let isCodeBlock = false
+    let codeBlockLanguage = ''
+    let codeLines = []
+
+    for (let i = 0; i < lines.length; i++) {
+      const line = lines[i]
+
+      if (line.startsWith('```')) {
+        if (!isCodeBlock) {
+          isCodeBlock = true
+          codeBlockLanguage = line.replace(/```(\S*)/, '$1')
+        } else {
+          isCodeBlock = false
+          formattedLines.push(
+            <div key={i} className="code-block">
+              <pre className={`language-${codeBlockLanguage}`}>
+                <code>{codeLines.join('\n')}</code>
+              </pre>
+            </div>
+          )
+          codeLines = []
+        }
+      } else if (isCodeBlock) {
+        codeLines.push(line)
+      } else {
+        formattedLines.push(<p key={i}>{line}</p>)
+      }
+    }
+
+    return formattedLines
+  }
 
   return (
     <div className={`p-4 ${createdBy === 'ai' ? 'bg-eerie  rounded-xl' : ''}`}>
-      {parsedText.map((line, index) => (
-        <React.Fragment key={index}>
-          {line}
-          <br />
-        </React.Fragment>
-      ))}
+      <div>{formatResponse(message)}</div>
     </div>
   )
 }
--- src/services/chat.ts
+++ src/services/chat.ts
@@ -1,31 +1,57 @@
+// import { getConfig } from './settings'
+
+import { HfInference } from '@huggingface/inference'
 import { getConfig } from './settings'
+
+// export async function queryChat(text: string): Promise<string> {
+//   const configData = await getConfig()
+//   const { apiKey, model, tokenLimit } = configData
+
+//   const response = await fetch(
+//     `https://api-inference.huggingface.co/models/${model}`,
+//     {
+//       headers: {
+//         Authorization: `Bearer ${apiKey}`,
+//         'Content-Type': 'application/json',
+//       },
+//       method: 'POST',
+//       body: JSON.stringify({
+//         inputs: queryChatBuilder(text),
+//         // inputs: text,
+//         parameters: {
+//           max_new_tokens: tokenLimit,
+//         },
+//       }),
+//     }
+//   )
+//   const result = await response.json()
+//   return queryChatCleaner(result[0].generated_text)
+//   // return result[0].generated_text
+// }
+
+// function queryChatBuilder(text: string): string {
+//   return `<|prompter|>${text}<|endoftext|><|assistant|>`
+// }
+
+// function queryChatCleaner(text: string): string {
+//   const response = text.split('<|assistant|>')
+//   return response[1]
+// }
 
 export async function queryChat(text: string): Promise<string> {
   const configData = await getConfig()
-  const { apiKey, model } = configData
+  const { apiKey, model, tokenLimit } = configData
 
-  const response = await fetch(
-    `https://api-inference.huggingface.co/models/${model}`,
-    {
-      headers: {
-        Authorization: `Bearer ${apiKey}`,
-        'Content-Type': 'application/json',
-      },
-      method: 'POST',
-      body: JSON.stringify({
-        inputs: queryChatBuilder(text),
-      }),
-    }
-  )
-  const result = await response.json()
-  return queryChatCleaner(result[0].generated_text)
-}
+  const hf = new HfInference(apiKey)
 
-function queryChatBuilder(text: string): string {
-  return `<|prompter|>${text}<|endoftext|><|assistant|>`
-}
+  const response = await hf.textGeneration({
+    model,
+    inputs: text,
+    parameters: {
+      max_new_tokens: tokenLimit,
+      return_full_text: false,
+    },
+  })
 
-function queryChatCleaner(text: string): string {
-  const response = text.split('<|assistant|>')
-  return response[1]
+  return response.generated_text
 }
